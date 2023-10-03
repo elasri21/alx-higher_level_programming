fb18ea4cc411229e66af2e5a6e5756847a038042
elasri21 <islame.edu@gmail.com>
2023-10-03 15:15:50 -0700

    ts 08

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,5 @@ def uppercase(str):
     for i in range(0, len(str)):
         order = ord(str[i])
         order = order - 32 if order >= 97 and order <= 122 else order
-        if i == len(str) - 1:
-            print("{}".format(chr(order)))
-        else:
-            print("{}".format(chr(order)), end="")
+        print("{}".format(chr(order)), end="")
+    print("")
