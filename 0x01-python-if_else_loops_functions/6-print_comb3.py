3a9d6e836b3097bc493661d4b0697e50fe366329
elasri21 <islame.edu@gmail.com>
2023-10-03 13:55:36 -0700

    ts 06

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for d1 in range(0, 10):
-    for d2 in range (d1 + 1, 10):
+    for d2 in range(d1 + 1, 10):
         if d1 == 8 and d2 == 9:
             print("{}{}".format(d1, d2))
         else:
