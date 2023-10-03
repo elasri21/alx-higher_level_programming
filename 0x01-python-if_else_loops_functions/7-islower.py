14cf72e91a67961bc5b2eed5892dacd12d0eb310
elasri21 <islame.edu@gmail.com>
2023-10-03 14:53:59 -0700

    ts 07

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,7 +3,9 @@ def islower(c):
     chs = ""
     for ch in range(97, 123):
         chs += chr(ch)
-    if c and c in chs:
+    if not  c:
+        return
+    if c in chs:
         return True
     else:
         return False
