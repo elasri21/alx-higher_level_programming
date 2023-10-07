46853a614880548500f8dcd4e2f33b36f24b9763
elasri21 <islame.edu@gmail.com>
2023-10-07 01:50:28 -0700

    ts 07

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        return (tuple_b)
-    if len(tuple_b) == 0:
-        return (tuple_a)
-    if len(tuple_a) == 1:
-        return ((tuple_a[0] + tuple_b[0], tuple_b[1]))
-    if len(tuple_b) == 1:
-        return ((tuple_a[0] + tuple_b[0], tuple_a[1]))
-    return ((tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]))
+    if isinstance(tuple_a, tuple) and isinstance(tuple_b, tuple):
+        if len(tuple_a) == 0:
+            return (tuple_b)
+        if len(tuple_b) == 0:
+            return (tuple_a)
+        if len(tuple_a) == 1:
+            return ((tuple_a[0] + tuple_b[0], tuple_b[1]))
+        if len(tuple_b) == 1:
+            return ((tuple_a[0] + tuple_b[0], tuple_a[1]))
+        return ((tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]))
