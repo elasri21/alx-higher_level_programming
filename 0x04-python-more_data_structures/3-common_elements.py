f98aad61a749b4b4e32ac2418a6f663b4c456483
elasri21 <islame.edu@gmail.com>
2023-10-11 03:59:43 -0700

    ts 03

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if len(set_1) > 0 and len(set_2):
+    if len(set_1) == 0 or len(set_2) == 0:
+        return ({})
+    if len(set_1) > 0 and len(set_2) > 0:
         set_3 = set_1.intersection(set_2)
     return (set_3)
