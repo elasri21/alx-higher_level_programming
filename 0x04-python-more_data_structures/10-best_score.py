8454312211c972a6ba8d45b1f5966623941c937b
elasri21 <islame.edu@gmail.com>
2023-10-11 10:39:12 -0700

    ts 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        if len(list(a_dictionary.keys())) == 0:
-            return (None)
         vs = list(a_dictionary.values())
         ks = list(a_dictionary.keys())
-        max_s = vs[0]
-        k_max = ""
-        for i in range(0, len(vs)):
-            if vs[i] > max_s:
-                max_s = vs[i]
-                k_max = ks[i]
-        return (k_max)
+        if len(ks) == 0:
+            return (None)
+        max_s = a_dictionary[ks[0]]
+        mx = ks[0]
+        for k in ks:
+            if a_dictionary[k] > max_s:
+                max_s = a_dictionary[k]
+                mx = k
+        return (mx)
