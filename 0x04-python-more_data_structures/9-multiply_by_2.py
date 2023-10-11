198d8704591d6c8ea62abdc179cd5b67609679b0
elasri21 <islame.edu@gmail.com>
2023-10-11 04:09:28 -0700

    ts 09

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if len(a_dictionary) > 0:
+    if a_dictionary:
         new_dic = dict()
         for k, v in a_dictionary.items():
             new_dic[k] = 2 * v
