25f01f57c5a0a73433ac57e93ef5c1188a0b0776
elasri21 <islame.edu@gmail.com>
2023-10-11 04:06:58 -0700

    ts 08

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    if key not in list(a_dictionary.keys()):
+        return (a_dictionary)
     if a_dictionary:
         for k in list(a_dictionary.keys()):
             if k == key:
