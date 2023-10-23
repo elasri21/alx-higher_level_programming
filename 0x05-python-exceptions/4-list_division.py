76e9fd6fdeba80bd3368607fbf6810c75ac7270f
elasri21 <islame.edu@gmail.com>
2023-10-23 12:30:40 -0700

    ts 04

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             res = 0
             print("division by 0")
         except IndexError:
-            rexs = 0
+            res = 0
             print("out of range")
         finally:
             new_list.append(res)
