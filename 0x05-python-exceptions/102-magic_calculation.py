726fd9cc064c5aac202479b0091335cf71109598
elasri21 <islame.edu@gmail.com>
2023-10-24 03:00:36 -0700

    ts 8

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,9 +3,9 @@ def magic_calculation(a, b):
     magic_res = 0
     for j in range(1, 3):
         try:
-            if i > a:
+            if j > a:
                 raise Exception('Too far')
-            magic_res += a ** b/i
+            magic_res += a ** b / j
         except Exception:
             magic_res = b + a
             break
