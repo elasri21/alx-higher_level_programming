61e3b09acc29fd410050586b1da0a3fdf9d6f6f9
elasri21 <islame.edu@gmail.com>
2023-11-06 02:43:06 -0800

    ts

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Contains a function that returnsmethods and properties
+of an object"""
+
 
 def lookup(obj):
+    """get all properties and methods of an object
+    Args:
+        obj: the object
+    Return: a list of all methods and properties"""
+
     return (list(dir(obj)))
