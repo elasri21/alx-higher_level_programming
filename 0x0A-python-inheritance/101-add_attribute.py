d12b45cd464b1216abf7df7ea28723c7401c46d4
elasri21 <islame.edu@gmail.com>
2023-11-06 11:00:53 -0800

    ts

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This Module contains function that adds an
+attribute to an object is it is possible"""
+
+
+def add_attribute(obj, att, val):
+    """adds an attribute to an object is it is possible
+    Args:
+        obj: the object
+        att: attribute to add
+        val: attribute value"""
+    if not hasattr(obj, '__dir__'):
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, val)
