b1792ed14b8aae10b4821af87259509fe9be8605
elasri21 <islame.edu@gmail.com>
2023-11-06 10:46:23 -0800

    ts

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Contains a class that inherits from int"""
+
+
+class MyInt(int):
+    def __eq__(self, other):
+        """dreverse the useul behavior
+        Args:
+            other: another class
+        Return: Fasle if other equal to self. True otherwise"""
+        return int(self) != other
+
+    def __ne__(self, other):
+        """dreverse the useul behavior
+        Args:
+            other: another class to compare with
+        Return: True if other equal to self. True otherwise"""
+        return int(self) == other
