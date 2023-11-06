3673d893a7dfa504f546a144b54bd8f54b3f85b6
elasri21 <islame.edu@gmail.com>
2023-11-06 09:55:27 -0800

    ts

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module contains square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A square class"""
+    def __init__(self, size):
+        """initializes a square
+        Args:
+            size: square side"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """calculates the square area"""
+        return (self.__size ** 2)
+
+    def __str__(self):
+        """print a readable format"""
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
