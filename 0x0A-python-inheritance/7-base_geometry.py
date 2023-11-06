8c956c2bd955b5ba34d051290053ed68e9ab5aca
elasri21 <islame.edu@gmail.com>
2023-11-06 08:29:18 -0800

    ts

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module contains BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """calucates the area of the shape"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates an integer
+        Args:
+            name: shape name
+            value: an intege"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
