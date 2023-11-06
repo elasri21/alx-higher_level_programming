e0704d50d25a355f41e9a73f71a726e1efa2f0a3
elasri21 <islame.edu@gmail.com>
2023-11-06 08:55:12 -0800

    ts

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,7 +14,7 @@ class BaseGeometry:
         Args:
             name: shape name
             value: an intege"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
