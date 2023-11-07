837ece03db9574ab5456bbf7ce4b44eca09aec2c
elasri21 <islame.edu@gmail.com>
2023-11-07 10:01:22 -0800

    ts

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""contains  a function that inserts a line of text to a file,
+after each line containing a specific string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file, after each
+    line containing a specific string
+    Args:
+        filename: the file name
+        search_string: the string pattern to look for
+        new_string: new string to insert"""
+    with open(filename, "w+") as f_name:
+        lines = f_name.readlines()
+        list_lines = []
+        for ln in lines:
+            if ln.find(search_string) != -1:
+                list_lines.append(ln)
+                list_lines.append("\n".join(new_string))
+            else:
+                list_lines.append(ln)
+        for new_line in list_line:
+            f_name.write(new_line)
