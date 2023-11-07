763a2e8275dde875d36c97a96731996353699296
elasri21 <islame.edu@gmail.com>
2023-11-07 02:55:04 -0800

    ts

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""contains a function that creates an Object from a “JSON file”"""
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a “JSON file”
+    Args:
+        filename: the file"""
+    with open(filename, "r") as f_name:
+        return (json.load(f_name.read()))
