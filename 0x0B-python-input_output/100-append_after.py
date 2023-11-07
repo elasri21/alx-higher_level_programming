48a88b39ad4b21172a7678035993cc7c2e0e9ef2
elasri21 <islame.edu@gmail.com>
2023-11-07 10:21:36 -0800

    ts

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,15 +10,15 @@ def append_after(filename="", search_string="", new_string=""):
         filename: the file name
         search_string: the string pattern to look for
         new_string: new string to insert"""
-    with open(filename, "r") as f_name:
+    with open(filename, "r", encoding='utf-8') as f_name:
         list_lines = []
         while True:
             line = f_name.readline()
             if line == "":
                 break
-            list_lines.append(line)
+            list_lines.append(line + "\n")
             if search_string in line:
                 list_lines.append(new_string)
-    with open(filename, "w") as f_name:
+    with open(filename, "w", encoding='utf-8') as f_name:
         for new_line in list_line:
             f_name.writelines(new_line)
