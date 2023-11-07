c78d6a2254a5208b69445f2cf90a3ff8c92503cd
elasri21 <islame.edu@gmail.com>
2023-11-07 09:02:20 -0800

    ts

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""contains the Student class"""
+
+
+class Student:
+    """This is a class student"""
+    def __init__(self, first_name, last_name, age):
+        """initializes an instance from Student
+        Args:
+            first_name: the persons first name
+            last_name: the person's last name
+            age: the person's age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance
+        Args:
+            attrs: list of strings
+        Return: a dictionary in json representation"""
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        new_dict = dict()
+        for k, v in self.__dict__.items():
+            if k in attrs:
+                new_dict[k] = v
+        return (new_dict)
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance
+        Args:
+            json: json data"""
+        for k,v in json.items():
+            if k in self.__dict__.keys():
+                self.__dict__[k] = v
