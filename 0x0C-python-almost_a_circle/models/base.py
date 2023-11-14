f7765fccfa7ade24c2080244f80804a5026a8b92
elasri21 <islame.edu@gmail.com>
2023-11-14 01:34:06 -0800

    ts

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -64,7 +64,10 @@ class Base:
             cls: the class
             dictionary: dictionary containing properties of cls
         Return: an instance with all attributes already set"""
-        dummy = cls(1, 1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ =="Square":
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
 
