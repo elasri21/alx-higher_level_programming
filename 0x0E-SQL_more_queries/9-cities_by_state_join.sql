195a7c7a48527ac38cbe38d344ae9cb74a1c2956
elasri21 <islame.edu@gmail.com>
2023-12-13 04:28:25 -0800

    ts

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 --  lists all cities contained in the database hbtn_0d_usa
 SELECT ct.id, ct.name, st.name
 FROM cities AS ct INNER JOIN states AS st on ct.state_id=st.id
-ORDER BY c.id;
+ORDER BY ct.id;
