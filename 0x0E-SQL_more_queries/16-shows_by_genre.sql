929de45e6522c6801fa740bec8050a4bba5ec9ee
elasri21 <islame.edu@gmail.com>
2023-12-13 08:45:35 -0800

    ts

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 --  lists all shows, all genres linked to that show, from hbtn_0d_tvshows.
 SELECT t.title, g.name
   FROM tv_shows AS t
-    LEFT JOIN tv_shows_genres AS s
+    LEFT JOIN tv_show_genres AS s
     ON t.id=s.show_id
     LEFT JOIN tv_genres AS g
     ON s.genre_id=g.id
