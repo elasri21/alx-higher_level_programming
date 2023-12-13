a2d1ede4a864acedfa900d41a9cc8f91696fa733
elasri21 <islame.edu@gmail.com>
2023-12-13 08:28:39 -0800

    ts

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,10 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of show.
-SELECT name, SUM(rate) AS rating
-FROM tv_genres AS g
-INNER JOIN tv_show_genres AS s
-ON s.genre_id=g.id
-INNER JOIN tv_show_ratings AS r
-ON r.show_id=s.show_id
-GROUP BY name order BY rating DESC;
+SELECT `name`, SUM(`rate`) AS `rating`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON s.`genre_id` = g.`id`
+
+       INNER JOIN `tv_show_ratings` AS r
+       ON r.`show_id` = s.`show_id`
+ GROUP BY `name`
+ ORDER BY `rating` DESC;
