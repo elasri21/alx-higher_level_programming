5d447ca62e42b79df82595942c7bdd3c05afea6b
elasri21 <islame.edu@gmail.com>
2023-12-13 03:03:25 -0800

    ts

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,6 +2,7 @@
 --(in the database hbtn_0d_usa) on the sql server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256) NOT NULL
+	id INT UNIQUE AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id)
 	);
