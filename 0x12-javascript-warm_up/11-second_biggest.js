c9f789422663e63acfd2b10ea7d28959f8164c6a
elasri21 <islame.edu@gmail.com>
2024-01-08 03:15:23 -0800

    ts

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,23 +1,30 @@
 #!/usr/bin/node
 const args = process.argv;
-const nums = [];
-for (let i = 2; i < args.length; i++) {
-  nums.push(parseInt(args[i]));
-}
 if (args.length > 3) {
-  let max = nums[0];
-  let sMax = nums[0];
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > max) {
-      max = nums[i];
+  const nums = [];
+  for (let i = 2; i < args.length; i++) {
+    if (!parseInt(args[i])) {
+      console.log(0);
+      break;
+    } else {
+      nums.push(parseInt(args[i]));
     }
   }
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > sMax && nums[i] < max) {
-      sMax = nums[i];
+  if (nums.length >= 2) {
+    let max = nums[0];
+    let sMax = nums[0];
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > max) {
+        max = nums[i];
+      }
     }
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > sMax && nums[i] < max) {
+        sMax = nums[i];
+      }
+    }
+    console.log(sMax);
   }
-  console.log(sMax);
 } else {
   console.log(0);
 }
