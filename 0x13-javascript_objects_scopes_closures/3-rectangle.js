a9bcbde9176c3971cd75e9f947d2d1c00fb5e916
elasri21 <islame.edu@gmail.com>
2024-01-09 03:51:29 -0800

    ts

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print = function () {
+    let row = '';
+    for (let i = 0; i < this.width; i++) row += 'X';
+    for (let i = 0; i < this.height; i++) console.log(row);
+  };
+};
