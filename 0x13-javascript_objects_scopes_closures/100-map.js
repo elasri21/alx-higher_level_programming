3ceb87a1645fb8f42d2fc853290917ad016ee112
elasri21 <islame.edu@gmail.com>
2024-01-09 07:53:48 -0800

    ts

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-newList = list.map((curr, index) => curr * index);
+const newList = list.map((curr, index) => curr * index);
 console.log(list);
 console.log(newList);
