f59122a971d9b77644bc9a8c3f55cfd2251d8497
elasri21 <islame.edu@gmail.com>
2024-01-09 08:34:34 -0800

    ts

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+const keys = Object.keys(dict);
+console.log(keys);
+keys.forEach(k => {
+  if (newDict[dict[k]]) newDict[dict[k]].push(k);
+  else newDict[dict[k]] = [k];
+});
