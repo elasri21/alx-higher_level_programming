2238962f86fba48c09ec8ff30314417727147e34
elasri21 <islame.edu@gmail.com>
2024-01-09 04:37:01 -0800

    ts

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint(c) {
+    if (c) {
+      for (let i = 0; i < this.width; i++) console.log(c.repeat(this.width));
+    } else {
+      this.print();
+    }
+  }
+};
