08f38e22ecf5bef1308b17cad158cc75f2f50676
elasri21 <islame.edu@gmail.com>
2024-01-26 01:38:13 -0800

    ts 00

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,16 @@
 """Contains a script that fetches data from a url"""
 import urllib.request
 
-
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        data = response.read()
+def main():
+    """The main function to be called"""
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        data = res.read()
         data_utf = data.decode('utf-8')
         print("Body response:")
         print("\t - type: {}".format(type(data)))
         print("\t - content: {}".format(data))
         print("\t - utf8 content: {}".format(data_utf))
+
+
+if __name__ == "__main__":
+    main()
