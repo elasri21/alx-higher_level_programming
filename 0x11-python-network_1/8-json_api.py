0eabdebdab2fe8f7d10c6c307ac4b130f77be9cf
elasri21 <islame.edu@gmail.com>
2024-01-26 03:27:09 -0800

    ts 07

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""contains a script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter"""
+import requests
+import sys
+
+
+def main(args):
+    """to be called
+    Args:
+        args: command line arguments"""
+    url = 'http://0.0.0.0:5000/search_user'
+    q = ''
+    if len(args) >= 2:
+        q = args[1]
+    data = {'q': a}
+    res = requests.post(url, data=data)
+    try:
+        result = res.json()
+        if bool(result) is False:
+            print("No result")
+        else:
+            print("[{}] {}".format(result['id'], result['name']))
+    except:
+        print('Not a valid JSON')
+
+
+if __name__ == "__main__":
+    main(sys.argv)
