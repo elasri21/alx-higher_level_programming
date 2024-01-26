f46e22b0da9ad9faa8171f5174bd62696d924466
elasri21 <islame.edu@gmail.com>
2024-01-26 01:35:19 -0800

    ts 00

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Contains a script that fetches data from a url"""
 import urllib.request
+
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         data = response.read()
         data_utf = data.decode('utf-8')
         print("Body response:")
-        print("\ttype: {}".format(type(data)))
-        print("\tcontent: {}".format(data))
-        print("\tutf8 content: {}".format(data_utf))
+        print("\t - type: {}".format(type(data)))
+        print("\t - content: {}".format(data))
+        print("\t - utf8 content: {}".format(data_utf))
