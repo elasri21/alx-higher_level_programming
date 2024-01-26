d6f3583806bacf39b535cc9c9a3dc8c1303d8daa
elasri21 <islame.edu@gmail.com>
2024-01-26 08:26:09 -0800

    ts 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,19 +4,18 @@ import requests
 import sys
 
 
-def main():
-    """to be called"""
-    repo_name = sys.argv[1]
-    owner = sys.argv[2]
-    url = f'https://api.github.com/repos/{owner}/{repo_name}'
-    res = requests.get(url)
+def fetch_commits(repo, owner):
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    res = requests.get(url)    
     if res.status_code == 200:
         commits = res.json()[:10]
         for commit in commits:
             sha = commit['sha']
             author = commit['commit']['author']['name']
-            print('{}: {}'.format(sha, author))
+            print("{}: {}".format(sha, author))
 
 
 if __name__ == "__main__":
-    main()
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    fetch_commits(repo, owner)
