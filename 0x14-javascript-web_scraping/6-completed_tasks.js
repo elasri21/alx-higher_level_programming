0095a40c71332caf1b9350d6e23d699d7562ce08
elasri21 <islame.edu@gmail.com>
2024-02-20 09:35:54 -0800

    ts

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  const agg = {};
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body);
+    data.forEach(el => {
+      if (el.completed) {
+        if (!agg[el.userId]) {
+          agg[el.userId] = 0;
+	}
+	agg[el.userId]++;
+      }
+    });
+    console.log(agg);
+  }
+});
