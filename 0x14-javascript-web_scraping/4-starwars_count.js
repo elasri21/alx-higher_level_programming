f13bcb774cee50951281c2eabedc5e747f1bebdc
elasri21 <islame.edu@gmail.com>
2024-02-20 09:13:24 -0800

    ts

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,16 +5,10 @@ request(url, (err, res, body) => {
   if (err) {
     console.log(err);
   } else {
-    let ct = 0;
-    const movie = JSON.parse(body).results;
-    for (let i = 0; i < movie.length; i++) {
-      const chars = movie[i].characters;
-      for (let j = 0; j < chars.length; j++) {
-        if (chars[j] === 'https://swapi-api.alx-tools.com/api/people/18/' || chars[j] === 'http://swapi-api.alx-tools.com/api/people/18/') {
-          ct++;
-        }
-      }
-    }
-    console.log(ct);
+    const movies = JSON.parse(body).results;
+    const charMovies = movies.filter(
+      c => c.characters.find(i => i.match(/\/people\/18\/?$/))
+    );
+    console.log(charMovies.length);
   }
 });
